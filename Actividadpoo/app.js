6034c1f0c07bd4d9a4cf1e72a43ecd73c19731e6
EdwindanielTIC <2024_edwin.ramirez@iticbcn.cat>
2025-02-21 19:22:41 +0100

    Añadiendo app,test

--- Actividadpoo/app.js
+++ Actividadpoo/app.js
@@ -1,19 +1,43 @@
+
 class app{
 
-    #tasques;
+    #tasques = [];
 
 
-    app(tasques){
-        this.#tasques = tasques;
+    afegirTasca(tasca){
+        this.#tasques.push(tasca);
+
+        console.log(`Tasca añadida: ${tasca.mostrarInfoTaca()}`);
     }
 
-
-    afegirTasca(tasca obj){
-
+    completaTasca(){
         
-
-
+        console.log(`La tasca am id: ${tasca.id}`);
     }
 
+    elimnarTasca(){
+        console.log(`Elimar Tasca amb id: ${tasca.id}`);
+
+    }
+    
+    modificaNomTasca(){
+        const tasca = this.#tasques.find(t => t.id === id);
+        if (tasca) {
+            tasca.nom = nouNom;
+            console.log(`Nom de la tasca modificat: ${tasca.mostrarInfoTasca()}`);
+        } else {
+            console.log(`Tasca amb ID ${id} no trobada.`);
+        }
+    }
+
+    actualizarLista(){
+        console.log("Llista de tasques actualitzada:");
+        this.#tasques.forEach(tasca => console.log(tasca.mostrarInfoTasca()));
+    }
+
+    mostrarTotalTasques() {
+        console.log(`Total de tasques: ${Tasca.obtenirTotalTasques()}`);
+    }
 
 }
+
