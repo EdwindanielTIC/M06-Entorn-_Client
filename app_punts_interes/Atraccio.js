15f03283e1c5404db9b9bf7e03d65c5123373867
EdwindanielTIC <2024_edwin.ramirez@iticbcn.cat>
2025-02-24 18:14:19 +0100

    Añadiendo Atraccio.js

--- app_punts_interes/Atraccio.js
+++ app_punts_interes/Atraccio.js
@@ -1,1 +1,32 @@
-atraccio
+class Atraccio extends PuntInteres{
+
+    horaris;
+    preu;
+    moneda;
+
+    constructor(id,pais,ciutat,nom,direccio,horari,preu,moneda){
+        super(id,pais,ciutat,nom,direccio);
+        this.horaris = horari;
+        this.preu = preu;
+        this.moneda = moneda;
+        
+    }
+
+    getPreuIva(ivaPais){
+
+        if(this.preu > 0){
+            console.log("Entrada gratuïta");
+        }else if(ivaPais == 0){
+            console.log(this.preu)
+        }else{
+            let resultadoIva ;
+
+          resultadoIva =  this.preu * (ivaPais/2) ;
+
+          return resultadoIva;
+
+        }
+    }
+
+
+}
