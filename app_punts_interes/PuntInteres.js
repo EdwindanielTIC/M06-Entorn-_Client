6aeb9dde27d18ca8a5b844bf40b0e6ef841abd0d
EdwindanielTIC <2024_edwin.ramirez@iticbcn.cat>
2025-02-24 17:10:19 +0100

    Añadiendo actividad PuntInteres.js

--- app_punts_interes/PuntInteres.js
+++ app_punts_interes/PuntInteres.js
@@ -1,2 +1,50 @@
-PuntInteres
+class PuntInteres{
+#id;
+#esManual = false;
+pais;
+ciutat;
+nom;
+direccio;
+tipus;
+latitud;
+longitud;
+puntuacio = 0;
+static totalTasques;
 
+
+constructor(id,pais,ciutat,nom,direccio){
+
+    this.#id = id;
+    this.pais = pais;
+    this.ciutat = ciutat;
+    this.nom = nom;
+    this.direccio = direccio;
+}
+
+
+getId(){
+
+    return this.#id;
+
+}
+
+setId(id){
+
+    this.#id = id;
+
+}
+
+getEsmanual(){
+
+}
+
+setEsmanual(){
+
+}
+
+static obtenirTotalElement(){
+
+}
+
+
+}
