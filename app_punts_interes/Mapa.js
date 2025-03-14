761d73e244fd8b173ec20664e08d81bffce86f8a
EdwindanielTIC <2024_edwin.ramirez@iticbcn.cat>
2025-03-14 19:47:02 +0100

    Mostrando mapa

--- app_punts_interes/Mapa.js
+++ app_punts_interes/Mapa.js
@@ -4,10 +4,39 @@ class Mapa{
     //pintara el mapa
     constructor(){
 
+        
+
+
     }
 
-    //mostrarPuntInicial() 
+    mostrarPuntInicial(){
+
+        this.#map = L.map('map', { center: [51.505, -0.09], zoom: 13 });
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+
+
+        L.marker([51.5, -0.09]).addTo(this.#map)
+            .bindPopup('Puntos iniciales')
+            .openPopup();
+      
+    } 
+
+    
+
+
     //ActualizarPunt()
     //borrarPunt()
     //#getPocisioActual()
 }
+
+
+const mapa1 = new Mapa();
+mapa1.mostrarPuntInicial()
+
+
+
+
+
