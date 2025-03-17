5ee38a8bccf0578894539ce578aeaad6465f5997
EdwindanielTIC <2024_edwin.ramirez@iticbcn.cat>
2025-03-17 17:14:17 +0100

    Contructor con mi ubicacion y Popu

--- app_punts_interes/Mapa.js
+++ app_punts_interes/Mapa.js
@@ -4,6 +4,60 @@ class Mapa{
     //pintara el mapa
     constructor(){
 
+        //inicializamos la variable que me dibujara el mapa
+        this.#map = L.map('map', { center: [51.505, -0.09], zoom: 13 });
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+
+        
+
+    L.marker([51.5, -0.09]).addTo(this.#map)
+            .bindPopup('Puntos iniciales')
+            .openPopup();
+
+
+    var circle = L.circle([51.508, -0.11], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(this.#map);
+
+    var polygon = L.polygon([
+        [51.509, -0.08],
+        [51.503, -0.06],
+        [51.51, -0.047]
+    ]).addTo(this.#map);
+
+
+//Api de geolocalizacion https://www.w3schools.com/JSREF/prop_geo_position.asp   informacion de donde la saque
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(
+            (pos) => {
+                const lat = pos.coords.latitude;
+                const lon = pos.coords.longitude;
+                this.#map.setView([lat,lon], 15);
+
+                L.marker([lat, lon]).addTo(this.#map)
+                .bindPopup('Puntos iniciales')
+                .openPopup();
+            },
+
+        (erro)=> {
+            console.log("Error en la geolocalizacion", erro);
+        }
+
+        )
+    }
+
+  
+
+
+
+    
+
         
 
 
@@ -11,16 +65,6 @@ class Mapa{
 
     mostrarPuntInicial(){
 
-        this.#map = L.map('map', { center: [51.505, -0.09], zoom: 13 });
-
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(this.#map);
-
-
-        L.marker([51.5, -0.09]).addTo(this.#map)
-            .bindPopup('Puntos iniciales')
-            .openPopup();
       
     } 
 
@@ -34,7 +78,8 @@ class Mapa{
 
 
 const mapa1 = new Mapa();
-mapa1.mostrarPuntInicial()
+
+
 
 
 
